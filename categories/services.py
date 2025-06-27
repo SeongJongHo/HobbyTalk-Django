0b234cdf0a6455b9e0da0f1b1cca6c023b03d2a6
SeongJongHo <whdgh9595@gmail.com>
2025-06-27 16:56:36 +0900

    ADD: 카테고리 서비스 작성

---
+++ categories/services.py
@@ -0,0 +1,44 @@
+from typing import Callable
+from categories.repositories import CategoryRepository, get_category_repository
+from common.exceptions import NotFoundException
+
+
+class CategoryService:
+    def __init__(self, category_repository: CategoryRepository):
+        self.category_repository = category_repository
+
+    def get_category(self, category_id: int) -> dict:
+        category = self.category_repository.find_by_id(category_id)
+        if not category:
+            raise NotFoundException(f"존재하지 않는 카테고리 입니다. id: {category_id}")
+        
+        return {
+            "id": category.id,
+            "name": category.name,
+            "created_at": category.created_at,
+            "updated_at": category.updated_at,
+        }
+    
+    def get_categories(self) -> list[dict]:
+        categories = self.category_repository.find_all()
+
+        return [
+            {
+                "id": category.id,
+                "name": category.name,
+                "created_at": category.created_at,
+                "updated_at": category.updated_at,
+            }
+            for category in categories
+        ]
+
+def categoryServiceFactory() -> Callable[[], CategoryService]:
+    _instance = None
+    def get_instance() -> CategoryService:
+        nonlocal _instance
+        if _instance is None:
+            _instance = CategoryService(get_category_repository())
+        return _instance
+    return get_instance
+
+get_category_service = categoryServiceFactory()
