9d81f30a04ab8a7ce2c483167a0944c1f5f8b6f7
SeongJongHo <whdgh9595@gmail.com>
2025-06-27 16:56:51 +0900

    ADD: 카테고리 뷰 작성

---
+++ categories/views.py
@@ -0,0 +1,17 @@
+from django.views import View
+
+from categories.services import get_category_service
+from common.response import ResponseGenerator, ResponseMsg
+
+
+class CategoryViewV1(View):
+    def __init__(self, category_service=get_category_service()):
+        self.category_service = category_service
+
+    def get(self, request, category_id=None):
+        result = None
+        if category_id:
+            result = self.category_service.get_category(category_id)
+        else: result = self.category_service.get_categories()
+
+        return ResponseGenerator.build(message=ResponseMsg.SUCCESS, data=result, status=200)
