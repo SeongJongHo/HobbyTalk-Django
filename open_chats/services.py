68d6f9680e41998782160e1bae6ea3efabd46c1d
SeongJongHo <whdgh9595@gmail.com>
2025-06-28 23:11:16 +0900

    MODIFY: 채팅 생성 서비스 중복 요청시 에러 반환으로 변경

--- open_chats/services.py
+++ open_chats/services.py
@@ -1,13 +1,13 @@
 from django.db import transaction
 
-from categories.services import CategoryService
+from categories.services import CategoryService, get_category_service
 from common.decorator import retryable
-from common.exceptions import LockAcquireException, NotFoundException, RetryException, TooManyCreateException
+from common.exceptions import LockAcquireException, NotFoundException, TooManyCreateException
 from common.repositories import LockRepository, get_lock_repository
 from common.security import PasswordEncoder
 from open_chats.dtos import CreateOpenChatRoomDto
 from open_chats.models import OpenChatRoomUser
-from open_chats.repositories import OpenChatRoomRepository, OpenChatRoomUserRepository
+from open_chats.repositories import OpenChatRoomRepository, OpenChatRoomUserRepository, get_open_chat_room_repository, get_open_chat_room_user_repository
 
 class OpenChatRoomService:
     MAX_CREATE_COUNT = 5
@@ -25,7 +25,6 @@ class OpenChatRoomService:
         self.lock_repository = lock_repository
         self.lock_key_prefix = "open_chat_room_lock:"
 
-    @retryable(max_retries=3)
     @transaction.atomic
     def create(self, user_id: int, dto: CreateOpenChatRoomDto) -> int:
         if not self.category_service.get_category(dto.category_id):
@@ -36,7 +35,7 @@ class OpenChatRoomService:
             hashed_password=PasswordEncoder.encode(dto.password) if dto.password else None,
         )
         
-        if(self.lock_repository.acquire_lock(self.generate_lock_key(user_id), str(open_chat_room.id), ex=3)):
+        if(self.lock_repository.acquire_lock(self.generate_lock_key(user_id), str(open_chat_room.id), ex=1)):
             try:
                 user_rooms = self.open_chat_room_user_repository.find_by_user_id(user_id)
                 if len(user_rooms) >= self.MAX_CREATE_COUNT:
@@ -46,7 +45,7 @@ class OpenChatRoomService:
                 self.open_chat_room_user_repository.save(OpenChatRoomUser(user_id=user_id, open_chat_room=open_chat_room))
             finally:
                 self.lock_repository.release_lock(self.generate_lock_key(user_id), str(open_chat_room.id))
-        else: raise RetryException("채팅방 생성 중 잠금 획득 실패")
+        else: raise LockAcquireException(f"채팅방 생성 중 다른 작업이 진행 중입니다. user_id: {user_id}, room_id: {open_chat_room.id}")
         
         return open_chat_room.id
 
@@ -55,3 +54,18 @@ class OpenChatRoomService:
             return f"{self.lock_key_prefix}{user_id}"
         return f"{self.lock_key_prefix}{key}:{user_id}"
         
+def get_open_chat_room_service_factory() -> OpenChatRoomService:
+    _instance = None
+    def get_instance() -> OpenChatRoomService:
+        nonlocal _instance
+        if _instance is None:
+            _instance = OpenChatRoomService(
+                open_chat_room_repository=get_open_chat_room_repository(),
+                open_chat_room_user_repository=get_open_chat_room_user_repository(),
+                category_service=get_category_service(),
+                lock_repository=get_lock_repository()
+            )
+        return _instance
+    return get_instance
+
+get_open_chat_room_service = get_open_chat_room_service_factory()
