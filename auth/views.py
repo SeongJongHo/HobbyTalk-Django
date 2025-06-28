3657cc742edbd15485fc03a2d4d1169ba5d65a47
SeongJongHo <whdgh9595@gmail.com>
2025-06-28 19:26:50 +0900

    ADD: 로그인 뷰 작성

--- auth/views.py
+++ auth/views.py
@@ -1,11 +1,11 @@
 from django.views import View
 
-from auth.dtos import SignUpDto
+from auth.dtos import SignInDto, SignUpDto
 from auth.services import AuthService, get_auth_service
 from common.decorator import validate_body
 from common.response import ResponseGenerator, ResponseMsg
 
-class AuthViewV1(View):
+class SignUpViewV1(View):
     def __init__(self, auth_service: AuthService=get_auth_service()):
         self.auth_service = auth_service
 
@@ -15,4 +15,19 @@ class AuthViewV1(View):
             message=ResponseMsg.CREATED,
             data={ 'user_id': self.auth_service.sign_up(request.validated) },
             status=201
+        )
+
+class SignInViewV1(View):
+    def __init__(self, auth_service: AuthService=get_auth_service()):
+        self.auth_service = auth_service
+    
+    @validate_body(SignInDto)
+    def post(self, request):
+        token_set = self.auth_service.sign_in(request.validated.username, request.validated.password)
+
+        return ResponseGenerator.build(
+            message=ResponseMsg.SUCCESS,
+            data={ 'access_token': token_set['access_token'] },
+            status=200,
+            refresh_token=token_set.get('refresh_token', None)
         )
