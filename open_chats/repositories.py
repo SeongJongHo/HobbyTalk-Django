786f348effaf91f2a0a3acd2d5a2568aca8cc849
SeongJongHo <whdgh9595@gmail.com>
2025-06-28 21:14:57 +0900

    ADD: 채팅방 리포지터리 작성

---
+++ open_chats/repositories.py
@@ -0,0 +1,18 @@
+from open_chats.models import OpenChatRoom, OpenChatRoomUser
+
+class OpenChatRoomRepository:
+    def find_by_id(self, room_id: int):
+        return OpenChatRoom.objects.filter(id=room_id, deleted_at=None).first()
+    
+    def find_by_user_id(self, user_id: int):
+        return list(OpenChatRoom.objects.filter(user_id=user_id, deleted_at=None).all())
+    
+    def save(self, open_chat_room: OpenChatRoom):
+        return open_chat_room.save()
+    
+class OpenChatRoomUserRepository:
+    def find_by_room_id_and_user_id(self, room_id: int, user_id: int):
+        return OpenChatRoomUser.objects.filter(id=room_id, user_id=user_id, deleted_at=None).first()
+    
+    def save(self, open_chat_room_user: OpenChatRoomUser):
+        return open_chat_room_user.save()
