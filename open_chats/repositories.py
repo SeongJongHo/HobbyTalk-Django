32f20609cdfd653bb96193ca3134273ea479c1c4
SeongJongHo <whdgh9595@gmail.com>
2025-06-29 01:13:48 +0900

    ADD: 내가 참여한 채팅방 목록 가져오기 리포지터리 작성

--- open_chats/repositories.py
+++ open_chats/repositories.py
@@ -1,11 +1,12 @@
-from open_chats.models import OpenChatRoom, OpenChatRoomUser
+import datetime
+from django.db.models import Prefetch, Max, Q, F
+from django.db.models.functions import Coalesce
+from django.db.models import DateTimeField
+from open_chats.models import OpenChat, OpenChatRoom, OpenChatRoomUser
 
 class OpenChatRoomRepository:
     def find_by_id(self, room_id: int):
         return OpenChatRoom.objects.filter(id=room_id, deleted_at=None).first()
-    
-    def find_by_user_id(self, user_id: int):
-        return list(OpenChatRoom.objects.filter(user_id=user_id, deleted_at=None).all())
     
     def save(self, open_chat_room: OpenChatRoom):
         return open_chat_room.save()
@@ -20,6 +21,55 @@ def get_open_chat_room_repository_factory() -> OpenChatRoomRepository:
     return get_instance
 
 get_open_chat_room_repository = get_open_chat_room_repository_factory()
+class ReadOpenChatRoomRepository:
+    def find_by_user_id(self, user_id: int, last_created_at: str, limit: int):
+        no_offset = 0
+        qs = OpenChatRoom.objects.filter(openchatroomuser__user_id=user_id, deleted_at=None)
+        qs = qs.annotate(
+            last_chat_created_at=Max(
+                'openchat__created_at',
+                filter=Q(openchat__deleted_at=None)
+            )
+        )
+        qs = qs.annotate(
+            last_activity=Coalesce(
+                'last_chat_created_at',
+                F('created_at'),
+                output_field=DateTimeField()
+            )
+        )
+        qs = qs.filter(last_activity__lt=last_created_at).order_by('-last_activity')[no_offset:limit]
+        qs = qs.select_related('category')
+        qs = qs.prefetch_related(
+            Prefetch(
+                'openchatroomuser_set',
+                queryset=OpenChatRoomUser.objects
+                    .filter(deleted_at=None)
+                    .select_related('user')
+                    .order_by('-created_at')[:5],
+                to_attr='recent_members'
+            ),
+            Prefetch(
+                'openchat_set',
+                queryset=OpenChat.objects
+                    .filter(deleted_at=None)
+                    .order_by('-created_at')[:1],
+                to_attr='last_chat'
+            ),
+        )
+
+        return list(qs)
+
+def get_read_open_chat_room_repository_factory() -> ReadOpenChatRoomRepository:
+    _instance = None
+    def get_instance() -> ReadOpenChatRoomRepository:
+        nonlocal _instance
+        if _instance is None:
+            _instance = ReadOpenChatRoomRepository()
+        return _instance
+    return get_instance
+
+get_read_open_chat_room_repository = get_read_open_chat_room_repository_factory()
 
 class OpenChatRoomUserRepository:
     def find_by_room_id_and_user_id(self, room_id: int, user_id: int):
