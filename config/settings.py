1b13a1a22d1351d203ace5ab894ff7d165772512
SeongJongHo <whdgh9595@gmail.com>
2025-07-05 21:46:27 +0900

    ADD: ASGI 설정 추가

--- config/settings.py
+++ config/settings.py
@@ -100,7 +100,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-
+ASGI_APPLICATION = 'config.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
