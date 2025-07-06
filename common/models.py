ebef977286a3fa7e62c3bbca159b5eaba39beeea
SeongJongHo <whdgh9595@gmail.com>
2025-07-06 21:38:42 +0900

    ADD: 베이스 캐시 모델 작성

--- common/models.py
+++ common/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 from common.snowflake import generate_id
@@ -10,3 +11,15 @@ class BaseModel(models.Model):
     
     class Meta:
         abstract = True
+
+class BaseCacheModel():
+    id: int
+    created_at: datetime
+    updated_at: datetime
+    deleted_at: datetime
+
+    def __init__(self, id=generate_id(), created_at=None, updated_at=None, deleted_at=None):
+        self.id = id
+        self.created_at = created_at if created_at else datetime.now()
+        self.updated_at = updated_at if updated_at else datetime.now()
+        self.deleted_at = deleted_at if deleted_at else None
