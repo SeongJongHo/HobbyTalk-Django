23137c267b3140a6a334140beb96589ced58a871
SeongJongHo <whdgh9595@gmail.com>
2025-07-11 22:22:06 +0900

    ADD: 웹소켓 인증 미들웨어 작성

--- auth/middleware.py
+++ auth/middleware.py
@@ -1,5 +1,6 @@
 from auth.services import AuthService, get_auth_service
 from users.models import UserRole
+from channels.db import database_sync_to_async
 
 class AuthMiddleware:
     def __init__(self, get_response, auth_service: AuthService=get_auth_service()):
@@ -28,4 +29,62 @@ class AuthMiddleware:
         return {
             'user_id': None,
             'role': UserRole.UNKNOWN,
+        }
+    
+def parse_cookies(headers: list[tuple[bytes, bytes]]) -> dict:
+    cookies = {}
+    for name, value in headers:
+        if name == b"cookie":
+            cookie_str = value.decode()
+            items = cookie_str.split(";")
+            for item in items:
+                if "=" in item:
+                    k, v = item.strip().split("=", 1)
+                    cookies[k] = v
+    return cookies
+
+class WebSocketAuthMiddleware:
+    def __init__(self, inner, auth_service: AuthService = get_auth_service()):
+        self.inner = inner
+        self.auth_service = auth_service
+
+    def __call__(self, scope):
+        return WebSocketAuthMiddlewareInstance(scope, self.inner, self.auth_service)
+
+class WebSocketAuthMiddlewareInstance:
+    def __init__(self, scope, inner, auth_service):
+        self.scope = scope
+        self.inner = inner
+        self.auth_service = auth_service
+
+    async def __call__(self, receive, send):
+        headers = dict((k.decode(), v.decode()) for k, v in self.scope["headers"])
+        cookies = parse_cookies(self.scope["headers"])
+
+        access_token = headers.get("authorization")
+        refresh_token = cookies.get("refresh_token")
+
+        try:
+            user = await self.authenticate_async(access_token, refresh_token)
+            self.scope["user"] = {
+                "user_id": user.get("user_id", 0),
+                "role": user.get("role", UserRole.UNKNOWN),
+            }
+        except Exception:
+            await send({
+                "type": "websocket.close",
+                "status": 4401
+            })
+            return
+
+        return await self.inner(self.scope, receive, send)
+
+    @database_sync_to_async
+    def authenticate_async(self, access_token, refresh_token):
+        return self.auth_service.authenticate(access_token, refresh_token)
+
+    def get_unknown_user(self):
+        return {
+            "user_id": None,
+            "role": UserRole.UNKNOWN,
         }
