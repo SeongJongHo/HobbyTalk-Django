c783aecabd973cd1a6798b0b061efc41309a0d67
SeongJongHo <whdgh9595@gmail.com>
2025-07-17 00:36:36 +0900

    MODIFY: 토큰 리프레시 컨트롤러 변경

--- auth/urls.py
+++ auth/urls.py
@@ -4,6 +4,6 @@ from auth.views import SignUpViewV1, SignInViewV1, TokenRefreshViewV1
 
 urlpatterns = [
     path('/sign-up', SignUpViewV1.as_view(), name='sign-up'),
-    path('/refresh-token', TokenRefreshViewV1.as_view(), name='refresh-token'),
+    path('/refresh-token', SignInViewV1.as_view(), name='refresh-token'),
     path('/sign-in', SignInViewV1.as_view(), name='sign-in'),
 ]
